961b91661df643fe2e528019795c8188de8a8ba8
Precious <kpcast48@harrisonburg.k12.va.us>
2019-09-17 09:13:39 -0400

    Update game.js
    Click

--- game.js
+++ game.js
@@ -4,11 +4,24 @@ postToPage(score); // Print to the page
 
 createButton('Button', btnPress); // Make a button
 changeTitle('Clicker Game');
+val(btnPress, 1000);
 
-setInterval(btnPress, 1000);
-
+setInter
 function btnPress() {
   score++;
   postToPage(score);
+
+
+
+  createButton('Button', btnPress); // Make a button
+
+
+function btnPress() {
+  score--
+  postToPage(score)
+  ;
+  }
+
+
 }
 
