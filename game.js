2e255a773bdfa86f851b85551753311389a33dc8
Precious <kpcast48@harrisonburg.k12.va.us>
2019-09-17 09:46:12 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ var score = 0;
 
 postToPage(score); // Print to the page
 
-createButton('Button', btnPress); // Make a button
+createButton('Bunnies', btnPress); // Make a button
 changeTitle('Clicker Game');
 val(btnPress, 1000);
 
@@ -10,18 +10,20 @@ setInter
 function btnPress() {
   score++;
   postToPage(score);
+ 
+createButton('Bunny', btnPress); // Make a button
 
-
-
-  createButton('Button', btnPress); // Make a button
-
-
-function btnPress() {
-  score--
-  postToPage(score)
-  ;
+function btnPress(){
+  score-
+  postToPage(score);
   }
+}
+
+{
+createButton('cat')
+if(score>20)
+then (change button Button 'cat')
+
 
 
 }
-
