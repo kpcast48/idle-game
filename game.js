b83fc0136ab66212593b9aafc4e0be980b86ecee
Precious <kpcast48@harrisonburg.k12.va.us>
2019-09-23 08:44:14 -0400

    Update game.js
    click

--- game.js
+++ game.js
@@ -1,12 +1,14 @@
 var score = 1; // Create a variable to store the score in
 
 var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('hello', btnPress);   // Create a button element
+var button = new Button('Carrots', btnPress);   // Create a button element
 var hidden = new Button('Boo!');
 hidden.hide();
 var scr = new Text(score);                    // Create a text element
 
 setInterval(btnPress, 1000);   // Set up a loop
+
+var button = new Button('Bunny', btnPress); 
 
 function btnPress() {
   score++;         // Increase the score
@@ -16,7 +18,7 @@ function btnPress() {
   }
   if(score > 15) {
     hidden.hide(500);
-  }
+  
 }
   
 
