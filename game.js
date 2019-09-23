20264e58b267d371cdf6cd884c469c13b1f8349d
Precious <kpcast48@harrisonburg.k12.va.us>
2019-09-23 09:54:05 -0400

    Update game.js
    Carrots

--- game.js
+++ game.js
@@ -1,36 +1,27 @@
-var score = 1; // Create a variable to store the score in
+var score = 0; // Create a variable to store the score in
 
 var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('Carrots', btnPress);   // Create a button element
-var hidden = new Button('Boo!');
-hidden.hide();
-var scr = new Text(score);                    // Create a text element
+var woodbutton = new Button('Wood', btnPress);   
+var carrotsbutton = new Button('carrots', btnPress);
+var carrots = o;
+var carrotcount 
+var Strawberriesbutton = new Button('strawberries', btnPress);
+var scr = new Text(score); // Create a text element
 
 setInterval(btnPress, 1000);   // Set up a loop
-
-var button = new Button('Bunny', btnPress); 
 
 function btnPress() {
   score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
-  if(score > 10) {
-    hidden.show();
-  }
-  if(score > 15) {
-    hidden.hide(500);
-  
-}
-  
+
+
 
   
   
  
 
 
-
-
   
- 
 }
 
 
