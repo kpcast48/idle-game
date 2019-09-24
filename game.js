b612df21d65c319c215ebc32fa8ae6bbb9f0869a
Precious <kpcast48@harrisonburg.k12.va.us>
2019-09-24 09:51:07 -0400

    Update game.js
    Click

--- game.js
+++ game.js
@@ -2,10 +2,20 @@ var score = 0; // Create a variable to store the score in
 
 var title = new Title('Welcome to the Game'); // Create a title element
 var woodbutton = new Button('Wood', btnPress);   
-var carrotsbutton = new Button('carrots',increasescore);
-var carrots = o;
-var carrotcount 
+var wood = 0;
+var woodcount 
+
+var carrotsbutton = new Button('carrots',btnPress);
+var carrots = 0;
+var carrotscount 
+
 var Strawberriesbutton = new Button('strawberries', btnPress);
+var strawberry = 0;
+var strawberriescount 
+
+var wheatbutton = new Button('wheat',btnPress);
+var wheat= 0;
+var wheatcount 
 var scr = new Text(score); // Create a text element
 
 setInterval(btnPress, 1000);   // Set up a loop
@@ -14,9 +24,14 @@ function btnPress() {
   score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
 
+  var text1 = new Text('I will go in column 1');
+  var rowSection = new Section('row'); // Create the row to store the columns in
+var column1 = new Section('col');    // Create the first column
+column1.add(text1);
 
-
-  
+var text2 = new Text('I will go in column 2');
+var column2 = new Section('col');    // Create the first column
+column2.add(text2);
   
  
 
