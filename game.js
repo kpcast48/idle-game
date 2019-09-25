afec37f828106d801bdc347e67475e6d878b8210
Precious <kpcast48@harrisonburg.k12.va.us>
2019-09-25 09:53:46 -0400

    More Materials

--- game.js
+++ game.js
@@ -1,21 +1,29 @@
 var score = 0; // Create a variable to store the score in
 
-var title = new Title('Welcome to the Game'); // Create a title element
-var woodbutton = new Button('Wood', btnPress);   
-var wood = 0;
-var woodcount 
+var title = new Title('Elysia Village'); // Create a title element
+var moneybutton = new Button('Money');   
+var Sellbutton = new Button('Sell');   
+
 
 var carrotsbutton = new Button('carrots',btnPress);
 var carrots = 0;
 var carrotscount 
 
-var Strawberriesbutton = new Button('strawberries', btnPress);
+var strawberriesbutton = new Button('strawberries', btnPress);
 var strawberry = 0;
 var strawberriescount 
 
 var wheatbutton = new Button('wheat',btnPress);
 var wheat= 0;
 var wheatcount 
+
+var woodbutton = new Button('wood',btnPress);
+var wood= 0;
+var woodcount
+
+var woolbutton = new Button('wool',btnPress);
+var wool= 0;
+var woolcount
 var scr = new Text(score); // Create a text element
 
 setInterval(btnPress, 1000);   // Set up a loop
@@ -24,19 +32,19 @@ function btnPress() {
   score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
 
-  var text1 = new Text('I will go in column 1');
-  var rowSection = new Section('row'); // Create the row to store the columns in
-var column1 = new Section('col');    // Create the first column
-column1.add(text1);
+  let mySection = new Section();
+   var carrotsbutton = new Button('carrots');
+  var strawberriesbutton = new Button('strawberries');
+  var wheatbutton = new Button('wheat');
+  var woolbutton = new Button('wool');
 
-var text2 = new Text('I will go in column 2');
-var column2 = new Section('col');    // Create the first column
-column2.add(text2);
+  var buttonSection = new Section();
   
- 
+  buttonSection.add(carrotsbutton);
+  buttonSection.add(strawberriesbutton);
+  buttonSection.add(wheatbutton);
+  buttonSection.add(woolbutton);
 
-
-  
 }
 
 
