4e65d604f7424dda1f8a3a8abde463a58c910cff
kpcast48 <55092418+kpcast48@users.noreply.github.com>
2019-09-26 09:54:11 -0400

    Update game.js
    K

--- game.js
+++ game.js
@@ -1,10 +1,8 @@
 var score = 0; // Create a variable to store the score in
 
 var title = new Title('Elysia Village'); // Create a title element
-var moneybutton = new Button('Money');   
+var moneybutton = new Button('Money: ' + score);   
 var Sellbutton = new Button('Sell');   
-
-
 var carrotsbutton = new Button('carrots',btnPress);
 var carrots = 0;
 var carrotscount 
@@ -26,24 +24,34 @@ var wool= 0;
 var woolcount
 var scr = new Text(score); // Create a text element
 
-setInterval(btnPress, 1000);   // Set up a loop
+setInterval(btnPress, 1000);// Set up a loop
 
 function btnPress() {
   score++;         // Increase the score
-  scr.edit(score); // Update the page with the new score
+  moneybutton.edit("Money: " + score); // Update the page with the new score
+//materials section
+
+let score = 0; //Declare the data structure before using it!
+
+let Sellbutton = new Button('Click me to sell!', scoreIncrease); // Run the "score" function when you click the button
+
+// Don't forget to define the function!
+function scoreIncrease() {
+  score++;
+}
 
   let mySection = new Section();
    var carrotsbutton = new Button('carrots');
   var strawberriesbutton = new Button('strawberries');
   var wheatbutton = new Button('wheat');
-  var woolbutton = new Button('wool');
+  var wheatbutton = new Button('wheat')
+  var woodbutton = new Button('wood');
 
   var buttonSection = new Section();
-  
-  buttonSection.add(carrotsbutton);
-  buttonSection.add(strawberriesbutton);
-  buttonSection.add(wheatbutton);
-  buttonSection.add(woolbutton);
+
+
+
+
 
 }
 
